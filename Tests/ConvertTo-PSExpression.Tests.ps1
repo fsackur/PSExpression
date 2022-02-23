2dcab5c89a0cd7587f4990d419dfc85cd5b1976b
Freddie Sackur <github@dustyfox.uk>
2022-02-23 20:37:45 +0000

    Expand test cases

--- Tests/ConvertTo-PSExpression.Tests.ps1
+++ Tests/ConvertTo-PSExpression.Tests.ps1
@@ -2,69 +2,48 @@
 BeforeDiscovery {
 
     $PrimitiveTestCases = @(
-        '$null',
-        @{Name = '$true';  InputObject = $true;  Expected = '$true'},
-        @{Name = '$false'; InputObject = $false; Expected = '$false'}
-        '0',
-        '1',
-        '-1',
-        '0.5',
-        '-0.5',
-        '1/3',
-        '[Math]::PI',
-        '[uint16]::MinValue',
-        '[uint16]::MaxValue',
-        '[long]::MinValue',
-        '[long]::MaxValue',
-        '[int]::MinValue',
-        '[int]::MaxValue',
-        '[float]::MinValue',
-        '[float]::MaxValue',
-        '[double]::MinValue',
-        '[double]::MaxValue',
-        "[char]'c'"
-    ) | ForEach-Object {
-        if ($_ -is [hashtable])
-        {
-            return $_
-        }
-        $InputObject = Invoke-Expression $_
-        @{
-            Name        = $_
-            InputObject = $InputObject
-            Expected    = if ($null -eq $InputObject) {$_} else {$InputObject.ToString()}
-        }
-    }
+        @{Name = '$null';               InputObject = $null;                Expected = '$null'},
+        @{Name = '$true';               InputObject = $true;                Expected = '$true'},
+        @{Name = '$false';              InputObject = $false;               Expected = '$false'},
+        @{Name = "[char]'c'";           InputObject = [char]'c';            Expected = 'c'},
+        @{Name = '0';                   InputObject = 0;                    Expected = '0'},
+        @{Name = '1';                   InputObject = 1;                    Expected = '1'},
+        @{Name = '-1';                  InputObject = -1;                   Expected = '-1'},
+        @{Name = '0.5';                 InputObject = 0.5;                  Expected = '0.5'},
+        @{Name = '-0.5';                InputObject = -0.5;                 Expected = '-0.5'},
+        @{Name = '[uint16]::MinValue';  InputObject = [uint16]::MinValue;   Expected = '0'},
+        @{Name = '[uint16]::MaxValue';  InputObject = [uint16]::MaxValue;   Expected = '65535'},
+        @{Name = '[long]::MinValue';    InputObject = [long]::MinValue;     Expected = '-9223372036854775808'},
+        @{Name = '[long]::MaxValue';    InputObject = [long]::MaxValue;     Expected = '9223372036854775807'},
+        @{Name = '[int]::MinValue';     InputObject = [int]::MinValue;      Expected = '-2147483648'},
+        @{Name = '[int]::MaxValue';     InputObject = [int]::MaxValue;      Expected = '2147483647'},
+
+        # Precision is system-dependant
+        @{Name = '1/3';                 InputObject = 1/3;                  Expected = (1/3).ToString()},
+        @{Name = '[float]::MinValue';   InputObject = [float]::MinValue;    Expected = [float]::MinValue.ToString()},
+        @{Name = '[float]::MaxValue';   InputObject = [float]::MaxValue;    Expected = [float]::MaxValue.ToString()},
+        @{Name = '[double]::MinValue';  InputObject = [double]::MinValue;   Expected = [double]::MinValue.ToString()},
+        @{Name = '[double]::MaxValue';  InputObject = [double]::MaxValue;   Expected = [double]::MaxValue.ToString()},
+        @{Name = '[Math]::PI';          InputObject = [Math]::PI;           Expected = [Math]::PI.ToString()}
+    )
 
     $PrimitiveErrorCases = (
-        ('[float]::NaN', "'NaN' of type System.Single"),
-        ('[double]::NaN', "'NaN' of type System.Double"),
-        ('[float]::NegativeInfinity', "value '-∞' of type System.Single"),
-        ('[double]::PositiveInfinity', "value '∞' of type System.Double")
-    ) | ForEach-Object {
-        $InputObject = Invoke-Expression $_[0]
-        @{
-            Name        = $_[0]
-            InputObject = $InputObject
-            Expected    = $_[1]
-        }
-    }
+        @{Name = '[float]::NaN';                InputObject = [float]::NaN;                 ExpectedError = "value 'NaN' of type System.Single"},
+        @{Name = '[double]::NaN';               InputObject = [double]::NaN;                ExpectedError = "value 'NaN' of type System.Double"},
+        @{Name = '[float]::NegativeInfinity';   InputObject = [float]::NegativeInfinity;    ExpectedError = "value '-∞' of type System.Single"},
+        @{Name = '[double]::PositiveInfinity';  InputObject = [double]::PositiveInfinity;   ExpectedError = "value '∞' of type System.Double"}
+    )
 
+    $PartyTime = [datetime]::new(1999, 12, 31, 23, 59, 59, 999, 'Utc')
     $ObjectTestCases = @(
-        ('singleline', "'singleline'"),
-        ("Windows`r`nmultiline", "'Windows`r`nmultiline'"),
-        ("Linux`nmultiline", "'Linux`nmultiline'"),
-        ([datetime]::new(1999, 12, 31, 23, 59, 59, 999, 'Utc'), "[datetime]'1999-12-31 23:59:59Z'"),
-        ([version]'1.2.3.4', "[version]'1.2.3.4'"),
-        ([DateTimeKind]::Utc, "'Utc'"),
-        ({Get-ChildItem foo}, '{Get-ChildItem foo}')
-    ) | ForEach-Object {
-        @{
-            Name        = $_[0].ToString()
-            InputObject = $_[0]
-            Expected    = $_[1]
-        }
-    }
+        @{Name = 'singleline';              InputObject = 'singleline';             Expected = "'singleline'"},
+        @{Name = "Windows`r`nmultiline";    InputObject = "Windows`r`nmultiline";   Expected = "'Windows`r`nmultiline'"},
+        @{Name = "Linux`nmultiline";        InputObject = "Linux`nmultiline";       Expected = "'Linux`nmultiline'"},
+        @{Name = '[datetime]$PartyTime';    InputObject = $PartyTime;               Expected = "[datetime]'1999-12-31 23:59:59Z'"},
+        @{Name = "[version]'1.2.3.4'";      InputObject = [version]'1.2.3.4';       Expected = "[version]'1.2.3.4'"},
+        @{Name = '[DateTimeKind]::Utc';     InputObject = [DateTimeKind]::Utc;      Expected = "'Utc'"},
+        @{Name = '{Get-ChildItem foo}';     InputObject = {Get-ChildItem foo};      Expected = '{Get-ChildItem foo}'}
+    )
 }
 
 
@@ -81,7 +60,7 @@ Describe "ConvertTo-PSExpression" {
 
         It "Throws on <Name>" -ForEach $PrimitiveErrorCases {
 
-            {ConvertTo-PSExpression $InputObject} | Should -Throw *$Expected*
+            {ConvertTo-PSExpression $InputObject} | Should -Throw *$ExpectedError*
         }
     }
 
